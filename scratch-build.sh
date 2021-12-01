168fe304523971f2ab0d6b35e70c2172a4ce0ef5
Martin Cermak <mcermak@redhat.com>
2021-12-01 12:49:22 +0100

    Kick off systemtap configuration.

--- scratch-build.sh
+++ scratch-build.sh
@@ -1,80 +1,89 @@
 #!/bin/bash
 
 # Scratch-build components in a side-tag
+set -e
 
-# List the components to rebuild
-COMPONENT_LIST="kernel lua opencryptoki"
-
-# Parameters:
-# $1 - NVR
-# $2 - target release, e.g.: 'f34'
-
-# Required environment variables:
-# KOJI_KEYTAB - path to the keytab that can be used to build packages in Koji
-# KRB_PRINCIPAL - kerberos principal
-# DIST_GIT_URL - dist-git URL
-# ARCH_OVERRIDE - (optional) only build for specified architectures (example: "x86_64,i686")
+# tools
+rhpkg=fedpkg
+brew=koji
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0  <nvr> <release>"
-    exit 101
+    echo "Manually running scratch-build test..."
+    echo "----------------------------------------------------------------"
+    read -p "NVR (e.g. gcc-11.2.1-6.1.el9) : " nvr
+    read -p "target release (e.g. rhel-9.0.0) : " release
+else
+    set -x
+
+    # Parameters:
+    # $1 - NVR
+    # $2 - target release, e.g.: 'f34'
+    nvr=${1}
+    release=${2}
+
+    # Required environment variables:
+    # KOJI_KEYTAB - path to the keytab that can be used to build packages in Koji
+    # KRB_PRINCIPAL - kerberos principal
+    # DIST_GIT_URL - dist-git URL
+    # ARCH_OVERRIDE - (optional) only build for specified architectures (example: "x86_64,i686")
+    env | sort
+
+    if [ -z "${KOJI_KEYTAB}" ]; then
+        echo "Missing keytab, cannot continue..."
+        exit 101
+    fi
+
+    if [ -z "${KRB_PRINCIPAL}" ]; then
+        echo "Missing kerberos principal, cannot continue..."
+        exit 101
+    fi
+
+    kinit -k -t ${KOJI_KEYTAB} ${KRB_PRINCIPAL}
 fi
 
-nvr=${1}
-release=${2}
-
-build_target="${release}-build"
-
-
-if [ -z "${KOJI_KEYTAB}" ]; then
-    echo "Missing keytab, cannot continue..."
-    exit 101
-fi
-
-if [ -z "${KRB_PRINCIPAL}" ]; then
-    echo "Missing kerberos principal, cannot continue..."
-    exit 101
-fi
-
-set -e
 set -x
 
-kinit -k -t ${KOJI_KEYTAB} ${KRB_PRINCIPAL}
+# components under rebuild test
+if echo ${nvr} | fgrep -q systemtap; then
+    components="glibc qemu"
+else
+    components="kernel lua opencryptoki strace"
+fi
 
 # create a new side-tag
-sidetag_name=$(fedpkg request-side-tag --base-tag ${build_target} | grep ' created.$' | awk -F\' '{ print $2 }')
+build_target="${release}-build"
+sidetag_name=$(${rhpkg} request-side-tag --base-tag ${build_target} | grep ' created.$' | awk -F\' '{ print $2 }')
 date
 
 # tag the given NVR into the side-tag
-# this creates a task in koji, should be visible here:
-# https://koji.fedoraproject.org/koji/tasks?method=newRepo&state=active&view=tree&order=-id
-koji tag ${sidetag_name} ${nvr}
+${brew} tag ${sidetag_name} ${nvr}
 date
 
 # wait for repo regeneration
-koji wait-repo --build ${nvr} ${sidetag_name}
+${brew} wait-repo --build ${nvr} ${sidetag_name} || \
+${brew} wait-repo --build ${nvr} ${sidetag_name}
 date
 
-# Run the scratchbuilds in parallel
-TMPFILES=""
-for comp in ${COMPONENT_LIST}; do
-    export t=$(mktemp /tmp/tmp-${comp}-XXXXXXXX)
-    TMPFILES="${TMPFILES} ${t}"
-    ( ./worker.sh ${comp} ${release} ${sidetag_name} |& tee ${t} ) &
-    unset t
+# scratch-build dependent component(s)
+buildlogs=""
+for component in ${components}; do
+    export buildlog=$(mktemp buildlog.${component}.XXXXXXXXXX)
+    buildlogs="${buildlogs} ${buildlog}"
+    ( ./worker.sh ${component} ${release} ${sidetag_name} |& tee ${buildlog} ) &
+    unset %{buildlog}
 done
 wait
 date
 
-# Check for build errors
-EXIT_CODE=0
-for t in ${TMPFILES}; do
-    e=$(awk -f parse.awk ${t})
-    EXIT_CODE=$((EXIT_CODE + e))
-    rm ${t}
-done
+exit_code=$(awk -f parse.awk ${buildlogs})
 
-# remove the side-tag as it is no longer needed
-fedpkg remove-side-tag ${sidetag_name}
+# show the buildsystem task URLs
+grep 'https:\/\/[^\ ]*'  ${buildlogs}
 
-exit ${EXIT_CODE}
+rm ${buildlogs}
+
+# try to remove the side-tag as it is no longer needed
+${rhpkg} remove-side-tag ${sidetag_name} ||:
+
+exit ${exit_code}
+
