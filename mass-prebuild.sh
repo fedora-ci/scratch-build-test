134809655eff20a99cce4dfae188b7666ca56b16
Martin Cermak <mcermak@redhat.com>
2023-03-31 18:28:39 +0200

    Experiment cont'd

--- mass-prebuild.sh
+++ mass-prebuild.sh
@@ -2,63 +2,11 @@
 
 set -xe
 
-true ==========================================================================
-
-env | sort
-
-true ==========================================================================
-
-cat /etc/os-release
-
-true ==========================================================================
-
-# cat > /etc/yum.repos.d/mass-prebuild.repo <<EOF1
-# [copr:copr.fedorainfracloud.org:fberat:mass-prebuild]
-# name=Copr repo for mass-prebuild owned by fberat
-# baseurl=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/epel-8-\$basearch/
-# type=rpm-md
-# skip_if_unavailable=True
-# gpgcheck=1
-# gpgkey=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/pubkey.gpg
-# repo_gpgcheck=0
-# enabled=1
-# enabled_metadata=1
-# EOF1
-# 
-# 
-# yum -y --enablerepo=epel install /usr/bin/mpb ||:
-# 
-# c='/etc/ssh/sshd_config'
-# sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' $c;
-# systemctl restart sshd.service
-# echo -e "redhat\nredhat\n" | passwd root
-# ip a | fgrep glo
-# set +x
-# while true; do
-#     sleep 1
-#     test -f /goon && break
-# done
-
-# if [ -z "${KOJI_KEYTAB}" ]; then
-#     echo "Missing keytab, cannot continue..."
-#     exit 101
-# fi
-# 
-# if [ -z "${KRB_PRINCIPAL}" ]; then
-#     echo "Missing kerberos principal, cannot continue..."
-#     exit 102
-# fi
-# 
-# kinit -k -t ${KOJI_KEYTAB} ${KRB_PRINCIPAL}
-# 
-# id
-
 mkdir -p ~/.config
 cat $COPR_CONFIG > ~/.config/copr
 
 cp -r .mpb ~/
 mpb ||:
 cat /root/.mpb/mpb.log
-
 
 exit 0
