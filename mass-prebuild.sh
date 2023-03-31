4547ac61404f19286f428d39f9919aeda83f7014
Martin Cermak <mcermak@redhat.com>
2023-03-31 13:57:29 +0200

    Experiment cont'd

--- mass-prebuild.sh
+++ mass-prebuild.sh
@@ -12,32 +12,35 @@ cat /etc/os-release
 
 true ==========================================================================
 
-cat > /etc/yum.repos.d/mass-prebuild.repo <<EOF1
-[copr:copr.fedorainfracloud.org:fberat:mass-prebuild]
-name=Copr repo for mass-prebuild owned by fberat
-baseurl=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/epel-8-\$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF1
+# cat > /etc/yum.repos.d/mass-prebuild.repo <<EOF1
+# [copr:copr.fedorainfracloud.org:fberat:mass-prebuild]
+# name=Copr repo for mass-prebuild owned by fberat
+# baseurl=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/epel-8-\$basearch/
+# type=rpm-md
+# skip_if_unavailable=True
+# gpgcheck=1
+# gpgkey=https://download.copr.fedorainfracloud.org/results/fberat/mass-prebuild/pubkey.gpg
+# repo_gpgcheck=0
+# enabled=1
+# enabled_metadata=1
+# EOF1
+# 
+# 
+# yum -y --enablerepo=epel install /usr/bin/mpb ||:
+# 
+# c='/etc/ssh/sshd_config'
+# sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' $c;
+# systemctl restart sshd.service
+# echo -e "redhat\nredhat\n" | passwd root
+# ip a | fgrep glo
+# set +x
+# while true; do
+#     sleep 1
+#     test -f /goon && break
+# done
 
-
-yum -y --enablerepo=epel install /usr/bin/mpb ||:
-
-c='/etc/ssh/sshd_config'
-sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' $c;
-systemctl restart sshd.service
-echo -e "redhat\nredhat\n" | passwd root
-ip a | fgrep glo
-set +x
-while true; do
-    sleep 1
-    test -f /goon && break
-done
-
+cp dejagnu-1.6.3-6.fc38.src.rpm /tmp/
+cp -r .mpb ~/
+mpb
 
 exit 0
