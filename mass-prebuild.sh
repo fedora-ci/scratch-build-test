929cae69c08afc8c21b25b61d9e5f8bfe3899a19
Martin Cermak <mcermak@redhat.com>
2023-04-04 13:51:07 +0200

    Experiment cont'd

--- mass-prebuild.sh
+++ mass-prebuild.sh
@@ -7,12 +7,41 @@ rm -rf ~/.cache/copr/*
 mkdir -p ~/.config
 cat $COPR_CONFIG > ~/.config/copr
 
-cp -r .mpb ~/
+#cp -r .mpb ~/
+# yum install -y expect
+# hostname
+# ip a
+# unbuffer mpb
+
+FEDRELEASE=38
+
+cat > work.sh <<EOFA
+#!/bin/bash
+cat /etc/redhat-release
+dnf -y copr enable fberat/mass-prebuild
+dnf -y install mass-prebuild copr-cli expect
 copr-cli whoami
-yum install -y expect
-hostname
-ip a 
 unbuffer mpb
+EOFA
+
+mkdir .mpb
+cat > .mpb/config <<EOFB
+packages:
+  colorgcc:
+    src_type: url
+    src: https://kojipkgs.fedoraproject.org//packages/colorgcc/1.4.5/20.fc39/src/colorgcc-1.4.5-20.fc39.src.rpm
+build_id: 0
+verbose: 5
+revdeps:
+  list:
+    - colorgcc
+EOFB
+
+rpm -q toolbox || \
+	dnf -y install --enablerepo=epel toolbox
+toolbox list | fgrep fedora-toolbox-${FEDRELEASE} || \
+	toolbox create --distro fedora --release ${FEDRELEASE}
+toolbox run --container fedora-toolbox-${FEDRELEASE} bash work.sh
 
 
 
